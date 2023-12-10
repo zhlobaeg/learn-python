ed405cc7c54c644dba028b54eaf970555c99c609
Yury Zhloba <yzh44yzh@yandex.ru>
2023-12-10 19:48:26 +0300

    добавили пару скинов

--- games/bombeman/generator.py
+++ games/bombeman/generator.py
@@ -5,9 +5,9 @@ import labyrinth
 class Generator(character.GameObject):
     def __init__(self, surface):
         super().__init__(surface)
-        self.color = (255, 255, 0)
-        # (x, y) = labyrinth.generate_coordinates(8)
-        self.x = 5 * 40
-        self.y = 5 * 40
+        self.set_skin(f'./images/generator.png')
+        (x, y) = labyrinth.generate_coordinates(8)
+        self.x = x
+        self.y = y
 
         
