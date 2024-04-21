48aed5755c9da0ab925056c83b222328209ada63
Egor Zhloba <zhlobaeg@yandex.ru>
2024-04-21 18:10:12 +0300

    изменили масштаб

--- color_selecter.py
+++ color_selecter.py
@@ -2,10 +2,10 @@ from tkinter import *
 
 root = Tk()
 root.title('rgb_colors')
-root.geometry("425x275")
+root.geometry("425x525")
 root.config(bg='black')
 
-canvas = Canvas(root, width=250, height=250)
+canvas = Canvas(root, width=250, height=500)
 canvas.grid(row=0, column=0, padx=10, pady=10)
 
 def make_rgb_color(red, green, blue):
@@ -18,11 +18,11 @@ def slider_changed(event):
     color = make_rgb_color(red, green, blue)
     canvas.configure(bg=color)
 
-slider_r = Scale(root, from_=0, to=255, length=250, background='darkgray', command=slider_changed)
+slider_r = Scale(root, from_=0, to=255, length=500, background='darkgray', command=slider_changed)
 slider_r.grid(row=0, column=2, sticky=N, pady=10)
-slider_g = Scale(root, from_=0, to=255, length=250, background='darkgray', command=slider_changed)
+slider_g = Scale(root, from_=0, to=255, length=500, background='darkgray', command=slider_changed)
 slider_g.grid(row=0, column=3, sticky=N, pady=10)
-slider_b = Scale(root, from_=0, to=255, length=250, background='darkgray', command=slider_changed)
+slider_b = Scale(root, from_=0, to=255, length=500, background='darkgray', command=slider_changed)
 slider_b.grid(row=0, column=4, sticky=N, pady=10)
 
 root.mainloop()
